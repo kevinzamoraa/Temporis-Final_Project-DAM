4b0fdc1925fc1514c4d50cd3f1df26180666d0da
Kevin Zamora <56316102+kevinzamoraa@users.noreply.github.com>
2026-04-29 23:44:21 +0200

    fixing feature - in progress / SplashScreen

--- app/src/main/java/com/kevinzamora/temporis_androidapp/SplashScreen.kt
+++ app/src/main/java/com/kevinzamora/temporis_androidapp/SplashScreen.kt
@@ -1,37 +1,21 @@
 package com.kevinzamora.temporis_androidapp
 
 import android.content.Intent
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.google.android.ads.mediationtestsuite.activities.HomeActivity
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
 import com.kevinzamora.temporis_androidapp.ui.auth.LoginActivity
-import com.google.firebase.auth.FirebaseAuth
 
-class SplashActivity : AppCompatActivity() {
-
+class SplashScreen : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+        // Instalamos la Splash Screen oficial antes de super.onCreate
+        val splashScreen = installSplashScreen()
+
         super.onCreate(savedInstanceState)
 
-        // Mostramos una vista de splash si tienes XML asignado (opcional)
-        setContentView(R.layout.activity_splash_screen)
-
-        // Esperamos unos milisegundos (opcional) y luego redirigimos
-        checkAuthState()
-    }
-
-    private fun checkAuthState() {
-        val currentUser = FirebaseAuth.getInstance().currentUser
-
-        if (currentUser != null) {
-            // Usuario autenticado → vamos a HomeActivity
-            val intent = Intent(this, HomeActivity::class.java)
-            startActivity(intent)
-        } else {
-            // Usuario NO autenticado → vamos a LoginActivity
-            val intent = Intent(this, LoginActivity::class.java)
-            startActivity(intent)
-        }
-
+        // Redirigir directamente al Login
+        val intent = Intent(this, LoginActivity::class.java)
+        startActivity(intent)
         finish()
     }
 }
