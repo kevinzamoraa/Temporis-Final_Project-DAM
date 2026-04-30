08f17da3db5b26ca9b1603d962048edb792edf34
Kevin Zamora <56316102+kevinzamoraa@users.noreply.github.com>
2026-04-30 16:06:05 +0200

    partial feature / Timer Unit Tests

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -106,6 +106,7 @@ dependencies {
     testImplementation(libs.junit)
     testImplementation("io.mockk:mockk:1.13.5")
     testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.1")
+    testImplementation("androidx.arch.core:core-testing:2.2.0")
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
 }
