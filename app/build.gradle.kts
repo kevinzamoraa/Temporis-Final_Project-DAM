b367c4c6d85d149b7a996cc6d5231ffe4f5e90c3
Kevin Zamora <56316102+kevinzamoraa@users.noreply.github.com>
2026-05-21 23:18:04 +0200

    fixed feature / build.gradle.kts / modifying code / solving Sonar importation issues and reestoring Sonar configuration from 'develop-testing-tofix' branch, which passed successfully all the process

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -3,7 +3,6 @@ plugins {
     alias(libs.plugins.kotlin.android)
     id("com.google.gms.google-services")
     id("jacoco")
-    // Forzamos la versión 5.1.0.4882 que es la que se instanció correctamente sin errores de beans
     id("org.sonarqube") version "5.1.0.4882"
 }
 
@@ -213,22 +212,21 @@ configurations.all {
 // =============================================================================
 sonar {
     properties {
+        property("sonar.projectBaseDir", projectDir.absolutePath)
+
         property("sonar.projectKey", "kevinzamoraa_Temporis-Final_Project-DAM")
         property("sonar.organization", "kevinzamoraa")
         property("sonar.host.url", "https://sonarcloud.io")
 
-        // 1. Indicamos explícitamente dónde está el código y los tests (Rutas relativas al módulo app)
-        property("sonar.sources", "app/src/main/java")
-        property("sonar.tests", "app/src/test/java")
+        // Al usar projectBaseDir, las rutas vuelven a ser relativas al módulo perfectamente
+        property("sonar.sources", "src/main/java")
+        property("sonar.tests", "src/test/java")
 
-        // 2. Indicamos los binarios compilados para evitar el error de instanciación de dependencias
-        property("sonar.java.binaries", "app/build/tmp/kotlin-classes/debug")
-        property("sonar.kotlin.binaries", "app/build/tmp/kotlin-classes/debug")
+        property("sonar.java.binaries", "build/tmp/kotlin-classes/debug")
+        property("sonar.kotlin.binaries", "build/tmp/kotlin-classes/debug")
 
-        // 3. Ruta al reporte XML usando un 'comodín' para JaCoCo
-        property("sonar.coverage.jacoco.xmlReportPaths", "**/build/reports/jacoco/**/*.xml")
+        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/testDebugUnitTestCoverageReport/testDebugUnitTestCoverageReport.xml")
 
-        // 4. Inyección segura del token de entorno para la inicialización
         property("sonar.token", System.getenv("SONAR_TOKEN") ?: "")
     }
 }
