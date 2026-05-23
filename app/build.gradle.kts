e9d95ea0ea7bedbea242995321a014714939cbc4
Kevin Zamora <56316102+kevinzamoraa@users.noreply.github.com>
2026-05-23 16:28:38 +0200

    fixed feature / build.gradle.kts (app) / delete Sonar config

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -61,17 +61,6 @@ android {
     }
 }
 
-// Configuración del escáner de Sonar enfocada de forma nativa en el submódulo
-sonar {
-    properties {
-        property("sonar.sources", "src/main/java")
-        property("sonar.tests", "src/test/java")
-        property("sonar.java.binaries", "build/tmp/kotlin-classes/debug")
-        property("sonar.kotlin.binaries", "build/tmp/kotlin-classes/debug")
-        property("sonar.coverage.jacoco.xmlReportPaths", "build/reports/jacoco/testDebugUnitTestCoverageReport/testDebugUnitTestCoverageReport.xml")
-    }
-}
-
 // =============================================================================
 // CONFIGURACIÓN UNIFICADA DE JACOCO Y TESTING (OPTIMIZADA)
 // =============================================================================
